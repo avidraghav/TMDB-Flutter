aacc4395dc10bcf7a387fce1da844d5e6b3882bd
avidraghav <raghavaggarwal776@gmail.com>
2023-09-21 15:48:39 +0530

    display fetched movies in PageView

--- lib/presentation/movies_screen.dart
+++ lib/presentation/movies_screen.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:tmdb_flutter/domain/model/movie.dart';
 import 'package:tmdb_flutter/domain/model/popular_movie_response.dart';
 import 'package:tmdb_flutter/domain/repository/movies_screen_repo.dart';
 
@@ -14,6 +15,7 @@ class MoviesScreen extends StatefulWidget {
 
 class _MoviesScreenState extends State<MoviesScreen> {
   late Future<PopularMovieResponse> moviesResponse;
+  final PageController _pageController = PageController(initialPage: 0);
 
   Dio configureDio() {
     final options = BaseOptions(
@@ -36,19 +38,52 @@ class _MoviesScreenState extends State<MoviesScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: FutureBuilder<PopularMovieResponse>(
-        future: moviesResponse,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Text(snapshot.data!.movies![1].title!);
-          } else if (snapshot.hasError) {
-            return Text('${snapshot.error}');
-          }
+        child: Column(
+          children: [
+            const SafeArea(child: Text("Trending Movies")),
+            Flexible(
+              child: FutureBuilder<PopularMovieResponse>(
+                future: moviesResponse,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return PageView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: snapshot.data!.movies!.length,
+                      controller: _pageController,
+                      itemBuilder: (BuildContext context, int index) =>
+                          Padding(
+                            padding: const EdgeInsets.all(80.0),
+                            child: MovieItem(movie: snapshot.data!.movies![index]),
+                          ),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text('${snapshot.error}');
+                  }
+                  // By default, show a loading spinner.
+                  return const Center(child: CircularProgressIndicator());
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-          // By default, show a loading spinner.
-          return const CircularProgressIndicator();
-        },
-      )),
+class MovieItem extends StatelessWidget {
+  const MovieItem({super.key, required this.movie});
+
+  final Movie movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 200,
+      height: 200,
+      child: Card(
+        child: Center(child: Text(movie.title.toString())),
+      ),
     );
   }
 }
